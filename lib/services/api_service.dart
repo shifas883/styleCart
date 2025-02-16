8b14d3c0199d3a8d1946646a2049a52babcc8404
shifas <shifasmeharnk@gmail.com>
2025-02-16 21:24:11 +0530

    last push

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -6,17 +6,6 @@ class ApiService {
   Future<List<Product>> fetchProducts() async {
     final response = await http.get(Uri.parse('https://fakestoreapi.com/products'));
 
-   try{
-     if (response.statusCode == 200) {
-       List<dynamic> data = json.decode(response.body);
-       return data.map((item) => Product.fromJson(item)).toList();
-     } else {
-       throw Exception('Failed to load products');
-     }
-   }
-   catch(e){
-     print("PPPPP$e");
-   }
     if (response.statusCode == 200) {
       List<dynamic> data = json.decode(response.body);
       return data.map((item) => Product.fromJson(item)).toList();
